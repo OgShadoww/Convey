1887c47f8e612818c44adb1d9879a640e1183c23
Orest <galenzao@gmail.com>
2026-02-23 17:53:06 +0000

    Change client

--- client/client.c
+++ client/client.c
@@ -16,47 +16,17 @@ int main() {
   if(inet_pton(AF_INET, "127.0.0.1", &addr.sin_addr) <= 0) return -1;
 
   if(connect(fd, &addr, sizeof(addr)) < 0) return -1;
+  
+  write(STDOUT_FILENO, "Hello \n Interface: \n 1.Login \n 2.Register 3.", strlen("Hello \n Interface: \n 1.Login \n 2.Register 3."));
 
-  MsgLogin l = {0};
-  strncpy(l.username, "Orest", sizeof(l.username) - 1);
-  strncpy(l.password, "123", sizeof(l.password) - 1);
-
-  // Compute exact payload size: u16 + username + u16 + password
-  size_t ul = strlen(l.username);
-  size_t pl = strlen(l.password);
-  size_t payload_needed = 2 + ul + 2 + pl;
-
-  Buff payload = {0};
-  payload.data = (uint8_t*)malloc(payload_needed);
-  if (!payload.data) {
-    perror("malloc");
-    return 1;
-  }
-  payload.len = payload_needed;
-  payload.pos = 0;
-
-  if (encode_payload_login(&payload, &l) < 0) {
-    fprintf(stderr, "encode_payload_login failed\n");
-    free(payload.data);
-    return 1;
+  char buff[128];
+  size_t n;
+  while((n =read(STDOUT_FILENO, buff, sizeof(buff)) != -1)) {
+    if(strcmp(buff, "login") == 0) {
+      
+    }
   }
 
-  // Set payload.len to bytes actually used
-  payload.len = payload.pos;
-  payload.pos = 0;
 
-  ConveyFrame f = {0};
-  f.header.magic = CONVEY_MAGIC;
-  f.header.version = CONVEY_VERSION;
-  f.header.type = MSG_AUTH_LOGIN;
-  f.payload = payload;
-
-  if (write_frame(fd, &f) < 0) {
-    fprintf(stderr, "write_frame failed\n");
-    free_frame(&f);
-    return 1;
-  }
-
-  free_frame(&f);
   return 0;
 }
