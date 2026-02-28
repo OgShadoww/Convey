be2802fc05b9cda88550e4226bcd2ebbabb40e30
Orest <galenzao@gmail.com>
2026-02-28 22:08:53 +0000

    Add new functions for non-blocking polling

--- protocol/include/protocol_transport.h
+++ protocol/include/protocol_transport.h
@@ -10,8 +10,12 @@
 ssize_t conn_read(int fd, void *buff, size_t n);
 ssize_t conn_write(int fd, const void *buff, size_t n);
 
-// Exact
+// Exact | Testing only
 int read_exact(int fd, void *buff, size_t len);
 int write_all(int fd, const void *buff, size_t len);
+
+// Reading some | To have non-blocking concurrency
+ssize_t read_some(int fd, void *buff, size_t len);
+ssize_t write_some(int fd, const void *buff, size_t len);
 
 #endif
